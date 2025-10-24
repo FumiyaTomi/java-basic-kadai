1057dc54afaf60d0fe03a982ac98c250b432346e
Fumiya Tomizawa <tomizawa.corporatetraining@gmail.com>
2025-10-24 17:12:52 +0900

    upload:課題

---
+++ kadai_018/src/kadai_018/Kato_Chapter18.java
@@ -0,0 +1,29 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+	public String familyName;
+	public String givenName;
+	public String address;
+
+	public Kato_Chapter18(){
+		this.familyName = "加藤";
+		this.givenName = "";
+		this.address = "東京都中野区〇×";
+	}
+	public void commonIntroduce() {
+		System.out.println("名前は"+this.familyName+this.givenName+"です");
+		System.out.println("住所は"+this.address+"です");
+
+	}
+
+	 abstract public void eachIntroduce();
+
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+		System.out.println("");
+
+	}
+}
+
+
