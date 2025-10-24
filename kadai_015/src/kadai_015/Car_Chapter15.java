5764bb263afe0603f0951a996772259e87f74d69
Fumiya Tomizawa <tomizawa.corporatetraining@gmail.com>
2025-10-24 15:03:18 +0900

    upload:課題

--- kadai_015/src/kadai_015/Car_Chapter15.java
+++ kadai_015/src/kadai_015/Car_Chapter15.java
@@ -4,21 +4,28 @@ public class Car_Chapter15 {
 	private int gear;
 	private int speed;
 	
-	public Car_Chapter15(int gear,int speed) {
-		this.gear = gear;
-		this.speed = speed;
+	public Car_Chapter15() {
+		this.gear = 1;
+		this.speed = 10;
 	}
 	public void changeGear(int afterGear) {
-		if(afterGear > 5) {	
-			this.gear = 1;
-			return;
+		switch(afterGear) {
+		case 1 -> this.speed=10;
+		case 2 -> this.speed = 20;
+		case 3 -> this.speed = 30;
+		case 4 -> this.speed = 40;
+		case 5 -> this.speed = 50;
+		default -> this.speed = 10;
 		}
+		
+		System.out.println("ギア"+gear+"から"+afterGear+"に切り替えました");
 		this.gear = afterGear; 
+
 		
 	}
 	
 	public void run() {
-		System.out.println("時速"+gear*speed+"km");
+		System.out.println("速度は時速"+speed+"kmです");
 	}
 
 }
